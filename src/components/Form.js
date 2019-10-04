9ad6f4dd57b9d9e653d8c630331b2f80abc706d8
David Ortega <david.ortega@habla.io>
2019-10-03 22:12:54 -0300

    initial commit

---
+++ src/components/Form.js
@@ -0,0 +1,23 @@
+import React, { useContext } from 'react';
+
+import Context from '../store/context';
+
+const Form = () => {
+  const { state, actions } = useContext(Context);
+
+  return (
+    <form action="submit">
+      <input
+        value={state.value}
+        onChange={e =>
+          actions({
+            type: 'setState',
+            payload: { ...state, value: e.target.value },
+          })
+        }
+      ></input>
+    </form>
+  );
+};
+
+export default Form;
---
+++ src/store/useGlobalState.js
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+
+const useGlobalState = () => {
+  const [state, setState] = useState({ value: 'Initial State', list: [] });
+
+  const actions = action => {
+    const { type, payload } = action;
+
+    switch (type) {
+      case 'setState':
+        return setState(payload);
+      default:
+        return state;
+    }
+  };
+
+  return { state, actions };
+};
+
+export default useGlobalState;
